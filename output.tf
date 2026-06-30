b964511db4a8dfbc4d43df5a9be232dfd67eef84
Mastafa Monoar <mastafa2618@gmail.com>
2026-06-30 11:02:06 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "cluster_name" {
-  value = aws_eks_cluster.eks.name
+value = aws_eks_cluster.eks.name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.eks.endpoint
+value = aws_eks_cluster.eks.endpoint
 }
 
 output "cluster_ca_certificate" {
-  value = aws_eks_cluster.eks.certificate_authority[0].data
+value = aws_eks_cluster.eks.certificate_authority[0].data
 }
 
 output "key_pair_name" {
-  value = aws_key_pair.DevOps.key_name
+value = "DevOps"
 }
